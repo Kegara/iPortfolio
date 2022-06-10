6aec7cb7dd77108f3c8bf7608cb65f93522a2e54
Kevin Garcia Ramirez <mr.kegara@gmail.com>
2022-06-10 02:35:10 -0500

    cleaning project

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,12 +142,12 @@
   }
 
   /**
-   * Skills animation
+   * Technologies animation
    */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
+  let TechnologiesContent = select('.Technologies');
+  if (TechnologiesContent) {
     new Waypoint({
-      element: skilsContent,
+      element: TechnologiesContent,
       offset: '80%',
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
@@ -209,35 +209,6 @@
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
     }
   });
 
